f6414a9e41b0346f0c301e2a1de1d01b9751cf0f
luckylinux <luckylinux>
2024-07-04 13:24:14 +0200

    Allow build as non-root. Removed call to setup.sh in build.sh.

--- build.sh
+++ build.sh
@@ -4,39 +4,52 @@
 relativepath="./" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing ${scriptpath}/${relativepath}); fi
 
-# Enable Developer Tools
-ENABLE_DEVEL="yes"
+# Build Root
+buildroot="${toolpath}/build"
 
-# Run Setup
-source ${toolpath}/setup.sh
+# Dist Root
+distroot="${toolpath}/dist"
 
-# Activate VENV
-source /opt/supermicro-fan-control/venv/bin/activate
+# Create venv
+python3 -m venv "${toolpath}/venv"
+
+# Activate venv
+source "${toolpath}/venv/bin/activate"
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install & Upgrade Development Tools
+pip install -r requirements.devel.txt
+pip install --upgrade -r requirements.devel.txt
+
+# Install Requirements (Echo)
+pip install -r requirements.prod.txt
 
 # Create Folders
-mkdir -p build
-mkdir -p dist
-mkdir -p tmp
-mkdir -p dist-onefile
+mkdir -p ${buildroot}
+mkdir -p ${distroot}
+mkdir -p ${buildroot}/build
+mkdir -p ${distroot}/dist
+mkdir -p ${distroot}/dist-onefile
 
 # Copy Sources
-cp "${toolpath}/opt/supermicro-fan-control/bin/supermicro-fan-control.py" "${toolpath}/tmp/supermicro-fan-control.py"
+cp "${toolpath}/opt/supermicro-fan-control/bin/supermicro-fan-control.py" "${buildroot}/supermicro-fan-control.py"
 
 # Change Directory to tmp Folder
-cd "${toolpath}/tmp" || exit
+cd "${buildroot}" || exit
 
 # Build using Nuitka
 python -m nuitka --standalone --follow-imports --onefile supermicro-fan-control.py
-
 
 # Build using Nuitka (needed in order to bypass some noexec Permission Issues ?)
 # https://github.com/Nuitka/Nuitka/issues/2246
 #python -m nuitka --standalone --follow-imports --onefile --onefile-tempdir-spec=/opt/supermicro-fan-control/tmp supermicro-fan-control.py
 
 # Move into the Respective Folders
-mv supermicro-fan-control.dist/* "${toolpath}/dist/"
-mv supermicro-fan-control.build/* "${toolpath}/build/"
-mv supermicro-fan-control.bin "${toolpath}/dist-onefile/"
+mv supermicro-fan-control.dist/* "${distroot}/dist/"
+mv supermicro-fan-control.build/* "${buildroot}/build/"
+mv supermicro-fan-control.bin "${distroot}/dist-onefile/"
 
 # Remove Temporary Folders
 rm -rf "supermicro-fan-control.dist"
--- conversion.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-speed = 100
-value = format(speed , "02x")
-print(f"Speed: {speed} % -> Value: 0x{value} HEX")
-
-maximum_speed_hex = 0x64
-maximum_speed_dec = format(maximum_speed_hex * 100/255)
-print(f"Maximum HEX Speed: {maximum_speed_hex} -> Maximum Value: {maximum_speed_dec}")
